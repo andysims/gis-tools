f1b352f195a5ad8a16b6029abebcd056bece7cda
Andy <nxttask@pm.me>
2026-03-13 11:10:11 -0700

    added some stuff to testing. This again, is a junk file.

--- tools/arcgis-cli/src/test.py
+++ tools/arcgis-cli/src/test.py
@@ -1,6 +1,41 @@
 # from .utils import to_yyyymmdd
 from utils import to_yyyymmdd as yd
 from config import load_env
+from arcgis_client import gis_connection
+import logging
 
-print(yd())
-load_env(source="portal")
+
+def gis_users(gis_conn) -> list[dict]:
+    users = gis_conn.users.search(max_users=15)
+
+    log.info(f"Users returned: {len(users)}")
+
+    user_list = [build_user_info(user) for user in users]
+
+    return user_list
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %I:%M:%S %p",
+    )
+
+    logging.getLogger("arcgis").setLevel(logging.ERROR)
+
+    print(yd())
+    print(load_env(source="portal"))
+
+    my_env = load_env(source="agol")
+    print(
+        gis_connection(
+            my_env.get("url"), my_env.get("username"), my_env.get("password")
+        )
+    )
+
+    gis_conn = gis_connection(
+        my_env.get("url"), my_env.get("username"), my_env.get("password")
+    )
+
+    gis_users(gis_conn=gis_conn)
