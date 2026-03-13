4f39234f41a02996b2e848243c2abf256143600f
Andy <nxttask@pm.me>
2026-03-12 18:13:12 -0700

    Working on CLI. Added main()

--- tools/arcgis-cli/src/cli.py
+++ tools/arcgis-cli/src/cli.py
@@ -2,7 +2,8 @@ import argparse
 import sys
 import logging
 
-logger = logging.getLogger(__name__)
+from config import load_env
+log = logging.getLogger(__name__)
 
 
 def parse_args():
@@ -11,7 +12,20 @@ def parse_args():
     parser.add_argument("--verbose", action="store_true", help="Enable debug logging")
 
     source_group = parser.add_mutually_exclusive_group(required=True)
-    source_group.add_argument("--portal", help="Flag searched ArcGIS Enterprise Portal")
-    source_group.add_argument("--agol", help="Flag searched ArcGIS Online")
+    source_group.add_argument("--portal", action="store_true", help="Flag searched ArcGIS Enterprise Portal")
+    source_group.add_argument("--agol", action="store_true", help="Flag searched ArcGIS Online")
 
     return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    
+    if args.portal:
+        load_env(source="portal")
+    elif args.agol:
+        load_env(source="agol")
+
+
+if __name__ == "__main__":
+    main()
