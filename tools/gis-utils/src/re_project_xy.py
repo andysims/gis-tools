bd59fc0463d8b5db7ee8b877762456652347c15b
Andy <nxttask@pm.me>
2026-03-14 11:08:27 -0700

    first commit

---
+++ tools/gis-utils/src/re_project_xy.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pyproj import Transformer
+import logging
+from pathlib import Path
+
+log = logging.getLogger(__name__)
+
+
+def re_project_columns(in_file: Path, in_epsg: int, out_epsg: int):
+    pass
