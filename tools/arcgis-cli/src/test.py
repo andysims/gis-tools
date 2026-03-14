ee4124b11cc55d6bad7ee215ae8fddfdf9d5c55f
Andy <nxttask@pm.me>
2026-03-14 09:29:33 -0700

    JUNK TEST FILE: portal isn't validating, agol is.

--- tools/arcgis-cli/src/test.py
+++ tools/arcgis-cli/src/test.py
@@ -4,6 +4,8 @@ from config import load_env
 from arcgis_client import gis_connection
 import logging
 
+log = logging.getLogger(__name__)
+
 
 def gis_users(gis_conn) -> list[dict]:
     users = gis_conn.users.search(max_users=15)
@@ -25,17 +27,8 @@ if __name__ == "__main__":
     logging.getLogger("arcgis").setLevel(logging.ERROR)
 
     print(yd())
-    print(load_env(source="portal"))
 
-    my_env = load_env(source="agol")
-    print(
-        gis_connection(
-            my_env.get("url"), my_env.get("username"), my_env.get("password")
-        )
-    )
-
+    my_env = load_env(source="portal")  # this would be passed in as args
     gis_conn = gis_connection(
         my_env.get("url"), my_env.get("username"), my_env.get("password")
     )
-
-    gis_users(gis_conn=gis_conn)
