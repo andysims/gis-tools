fecd34ee193b8f1cfca3566081d8153ceeddeada
Andy <nxttask@pm.me>
2026-03-14 19:53:31 -0700

    added more cli stuff

--- tools/arcgis-cli/src/cli.py
+++ tools/arcgis-cli/src/cli.py
@@ -1,8 +1,10 @@
 import argparse
 import sys
 import logging
-
 from config import load_env
+
+from arcgis_client import gis_connection
+from users import user_search
 
 log = logging.getLogger(__name__)
 
@@ -28,11 +30,25 @@ def parse_args():
 def main():
     args = parse_args()
 
-    if args.portal:
-        load_env(source="portal")
-    elif args.agol:
-        load_env(source="agol")
+    if args.search_user:
+        if args.agol:
+            gis_source = "AGOL"
+            env = load_env(source=gis_source)
+            gis = gis_connection(
+                org_url=env.get("url"),
+                username=env.get("username"),
+                password=env.get("password"),
+            )
+            user = user_search(gis=gis, identifier=args.search_user)
+        elif args.portal:
+            gis_source = "PORTAL"
+            env = load_env(source="portal")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %I:%M:%S %p",
+    )
     main()
