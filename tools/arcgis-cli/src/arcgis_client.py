c168e6875199301fcce1c34640a875992255687d
Andy <nxttask@pm.me>
2026-03-14 19:52:51 -0700

    Updated

--- tools/arcgis-cli/src/arcgis_client.py
+++ tools/arcgis-cli/src/arcgis_client.py
@@ -1,5 +1,5 @@
 import logging
-from arcgis.gis import GIS, User
+from arcgis.gis import GIS
 
 log = logging.getLogger(__name__)
 logging.getLogger("arcgis").setLevel(logging.ERROR)
@@ -23,34 +23,5 @@ def gis_connection(org_url: str, username: str, password: str) -> GIS:
 
     except Exception as e:
         log.error(f"Unexpected error connecting to {org_url}: {e}")
+        raise
         # raise GISConnectionError(f"Unexpected connection failure to {org_url}") from e
-
-
-def search_user(gis: GIS, identifier: str) -> User | None:
-    """
-    Finds user by either username OR email.
-
-    - If identifier contains '@', then email
-    - Otherwise, treated as username
-
-    Returns the User object if found, or None if not found.
-    """
-    if not identifier:
-        log.error("User identifier not provided")
-        return None
-
-    identifier = identifier.lower().strip()
-
-    if "@" in identifier:
-        results = gis.users.search(query=f"email:{identifier}", max_users=100)
-    else:
-        results = gis.users.search(query=f"{identifier}", max_users=100)
-
-    if len(results) > 1:
-        log.info(f"Multiple users found for {identifier}: {len(results)}")
-        for user in results:
-            print(user.fullName, user.email)
-        return results
-    else:
-        log.info(f"User found for: {identifier}")
-        return gis.users.get(identifier)
