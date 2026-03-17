35514499f518e4f49ad042926f34af4cb585f8b0
Andy <nxttask@pm.me>
2026-03-17 07:23:42 -0700

    Added to user search and returned results when user is searched

--- tools/arcgis-cli/src/reports.py
+++ tools/arcgis-cli/src/reports.py
@@ -1,16 +1,65 @@
-from arcgis.gis import Group
+from arcgis.gis import Group, User
 from utils import format_datetime
+from pathlib import Path
+from collections import OrderedDict
+import logging
+import pprint
+
+log = logging.getLogger(__name__)
 
 
-def simple_user_print(user):
+def simple_user_print(user: User):
     if isinstance(user, list):
-        print(f"Multiple users found:")
         for u in user:
-            print(f"  - {u.fullName}: {u.email} ({u.username})")
+            usr_info = get_user_details(user=u)
+            user_print_format(usr_info)
     elif user:
-        print(f"  - {user.fullName}: {user.email} ({user.username})")
-    else:
-        print("No user found.")
+        usr_info = get_user_details(user=user)
+        user_print_format(usr_info)
+
+
+def user_print_format(user: dict):
+    if not user:
+        log.warning("User not provided")
+        return
+
+    print(f"\n{user['fullName']}")
+    print("=" * len(user['fullName']))
+
+    for k, v in user.items():
+        if k != 'fullName':
+            print(f"- {k}: {v}")
+
+
+
+def get_user_details(user: User) -> dict:
+    # pprint.pprint(dict(user), indent=4)
+
+    # user_content = gis.content.search(query='owner:{}'.format(user.username), max_items=10000)
+    # content_count = len(user_content)
+
+    user_info = {
+        "fullName": user.get('fullName', ''),
+        "firstName": user.get('firstName', ''),
+        "lastName": user.get('lastName', ''),
+        "username": user.get('username', ''),
+        "idpUsername": user.get('idpUsername', 'N/A'),
+        "email": user.get('email', ''),
+        "created": format_datetime(user.get('created', '')),
+        "lastLogin": format_datetime(user.get('lastLogin', '')),
+        "role": user.get('role', ''),
+        "userLicenseTypeId": user.get('userLicenseTypeId', ''),
+        "provider": user.get('provider', ''),
+        # 'groups': user.get('groups', ''),
+        "groupCount": len(user.get('groups', '')),
+        "assignedCredits": user.get('assignedCredits', 'N/A'),
+        "availableCredits": user.get('availableCredits', 'N/A'),
+        "orgID": user.get('orgId', ''),
+    }
+
+    ordered_user_info = OrderedDict(user_info)
+    # pprint.pprint(user_info, indent=4)
+    return ordered_user_info
 
 
 def simple_group_print(group: Group):
@@ -18,7 +67,7 @@ def simple_group_print(group: Group):
     print(f"\t")
 
 
-def get_group_details(group: Group) -> dict:
+def get_group_details(group: Group) -> dict | None:
     group_members = group.get_members()
 
     group_owner = group.owner
@@ -47,31 +96,37 @@ def print_group_details(group: dict):
     print(f"\n{group['title']}")
     print("=" * len(group["title"]))
 
-    print(f"\t- Group ID: {group["id"]}")
-    print(f"\t- Created: {group["created"]}")
-    print(f"\t- Item/Content Count: {group['items']}")
-    print(f"\t- Members in group: {group['member_count']}")
+    print(f"- Group ID: {group["id"]}")
+    print(f"- Created: {group["created"]}")
+    print(f"- Item/Content Count: {group['items']}")
+    print(f"- Members in group: {group['member_count']}")
     
-    print(f"\t- Owner:")
-    print(f"\t\t- {group["owner"]}")
+    print("- Owner:")
+    print(f"\t- {group["owner"]}")
 
     group_admins_len = len(group['admins'])
     
     if not group['admins']:
-        print(f"\t- Admins: None")
+        print("- Admins: None")
     elif group_admins_len == 1:
-        print(f"\t- Admin: {group['admins'][0]}")
+        print("- Admin:")
+        print(f"\t- {group['admins'][0]}")
     elif group_admins_len > 1:
-        print(f"\t- Admins:")
+        print("- Admins:")
         for admin in group['admins']:
-            print(f"\t\t- {admin}")
+            print(f"\t- {admin}")
 
     users_len = len(group['users'])
     if not group['users']:
-        print(f"\t- Users: None")
+        print("- Users: None")
     elif users_len == 1:
-        print(f"\t- User: {group['users'][0]}")
+        print("- User:")
+        print(f"\t- {group['users'][0]}")
     elif users_len > 1:
-        print(f"\t- Users:")
+        print(f"- Users:")
         for user in group['users']:
-            print(f"\t\t- {user}")
+            print(f"\t- {user}")
+
+
+def export_csv_report(user_info: dict) -> Path:
+    pass
